d7de08c9f14641486ae87f3a320c1f230f796e44
desaishivam26 <shivamdesaixda@gmail.com>
2016-06-04 18:07:28 +0530

    osprey: update maintainer info

--- krexus_maintainer.mk
+++ krexus_maintainer.mk
@@ -1,3 +1,3 @@
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.krexus.maintainer="Chinmay_Kunkikar" \
-    ro.krexus.maintainer.info="Also_known_as_\"DarkFalcon\""
+    ro.krexus.maintainer="Chinmay_K,Shivam_D" \
+    ro.krexus.maintainer.info="Osprey_Maintainers"
